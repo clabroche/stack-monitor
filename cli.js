33e09622fd9d74e36ad020cf8c56841e0f2f40c9
clabroche <corentinlabroche@gmail.com>
2019-06-05 21:46:34 +0200

    fix(cli): name process

--- cli.js
+++ cli.js
@@ -1,5 +1,6 @@
 #!/bin/env node
 const path = require('path')
+process.title = "stack-monitor"
 let ________________stack_____________________ = [];
 try {
   if(process.argv[2]) {
@@ -8,6 +9,7 @@ try {
     chooseStackToLaunch()
   } else {
     console.error('Provide path to config as argument')
+    process.exit(1)
   }
 } catch (error) {
   throw new Error(path.resolve(process.argv[2]) + ' not found')
