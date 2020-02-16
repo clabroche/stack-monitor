29603b37c510a25e0e610c49334b8d99f7945691
clabroche <corentinlabroche@gmail.com>
2020-02-16 20:34:59 +0100

    fix:(socket): parse socket port on production file when you are on production env

--- server/models/socket.js
+++ server/models/socket.js
@@ -2,11 +2,20 @@ var express = require('express');
 var app = express.Router;
 const fs = require('fs')
 const path = require('path')
-const port = fs
-    .readFileSync(path.resolve( __dirname,'..','..','.env.local'), 'utf-8')
+let port
+if (process.env.NODE_ENV === "DEV") {
+  port = fs
+      .readFileSync(path.resolve( __dirname,'..','..','.env.local'), 'utf-8')
+      .split('\n')
+      .filter(line => line.includes('VUE_APP_SOCKET_PORT'))[0]
+      .split('=')[1]
+} else {
+  port = fs
+    .readFileSync(path.resolve(__dirname, '..', '..', '.env.production'), 'utf-8')
     .split('\n')
     .filter(line => line.includes('VUE_APP_SOCKET_PORT'))[0]
     .split('=')[1]
+}
 
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
