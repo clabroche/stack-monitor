eb52befa01e58b63f73a38e3aa2d1a54dc1f2fbe
clabroche <corentinlabroche@gmail.com>
2020-07-16 21:54:50 +0200

    fix(deepscan): resolve eslint error

--- server/app.js
+++ server/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -13,7 +12,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('*', (req, res) => {
