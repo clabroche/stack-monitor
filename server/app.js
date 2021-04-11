dc90a4ee82f20b49d436cc4ac1e88a4e8c2a1c90
clabroche <corentin.labroche@sailendra.fr>
2021-04-11 03:03:21 +0200

    chore(sonar): improve quality

--- server/app.js
+++ server/app.js
@@ -5,7 +5,6 @@ const indexRouter = require('./routes/index');
 
 const app = express();
 
-// app.use(logger('dev'));
 app.use(require('cors')());
 app.use(express.json({limit: '100mb'}));
 app.use(express.urlencoded({ extended: false }));
