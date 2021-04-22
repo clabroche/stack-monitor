b64e4527efa76692a82c851bdcf30977460efb48
clabroche <corentin.labroche@sailendra.fr>
2021-04-22 20:28:57 +0200

    fix(electron): add electron for vue

---
+++ server/bin/server.js
@@ -0,0 +1,109 @@
+
+const fs = require('fs')
+const path = require('path')
+process.title = "stack-monitor"
+
+/**
+ * Module dependencies.
+ */
+
+const app = require('../app');
+const http = require('http');
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+let port = normalizePort(process.env.PORT || '3000');
+
+if (process.env.NODE_ENV === "DEV") {
+  port = fs
+    .readFileSync(path.resolve(__dirname, '..', '..', '.env.local'), 'utf-8')
+    .split('\n')
+    .filter(line => line.includes('VUE_APP_SERVER_PORT'))[0]
+    .split('=')[1]
+} else {
+  port = fs
+    .readFileSync(path.resolve(__dirname, '..', '..', '.env.production'), 'utf-8')
+    .split('\n')
+    .filter(line => line.includes('VUE_APP_SERVER_PORT'))[0]
+    .split('=')[1]
+}
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  const bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  if (process.env.NODE_ENV !== "DEV" && !process.versions['electron']) {
+    require('open')('http://localhost:' + port)
+  }
+  console.log('Magic happens on ' + bind);
+}
