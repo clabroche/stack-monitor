24fe0428142b2a1c4a9ba1ef772f2ca622189341
clabroche <corentin.labroche@sailendra.fr>
2021-04-22 21:08:30 +0200

    fix(electron): add snapcraft config

--- vue.config.js
+++ vue.config.js
@@ -2,35 +2,38 @@ module.exports = {
   pluginOptions: {
     electronBuilder: {
       builderOptions: {
-        "appId": "com.electron.stackmonitor",
-        "extraFiles": [
+        appId: "com.electron.stackmonitor",
+        extraFiles: [
           {
-            "from": "./server",
-            "to": "./resources/app.asar.unpacked/server"
+            from: "./server",
+            to: "./resources/app.asar.unpacked/server",
           },
           {
-            "from": "./.env.production",
-            "to": "./resources/app.asar.unpacked/.env.production"
+            from: "./.env.production",
+            to: "./resources/app.asar.unpacked/.env.production",
           },
           {
-            "from": "./package-lock.json",
-            "to": "./resources/app.asar.unpacked/package.json"
-          }
+            from: "./package-lock.json",
+            to: "./resources/app.asar.unpacked/package.json",
+          },
         ],
-        "productName": "Stack Monitor",
-        "mac": {
-          "category": "your.app.category.type"
+        productName: "Stack Monitor",
+        mac: {
+          category: "your.app.category.type",
         },
-        "linux": {
-          "icon": "./electron/512x512.png",
-            "executableName": "StackMonitor",
-              "artifactName": "${productName}-${version}.${ext}",
-                "category": "Utility"
+        snap: {
+          publish: ['github']
         },
-        "win": {
-          "icon": "./electron/512x512.png"
-        }
-      }
-    }
-  }
-}
+        linux: {
+          icon: "./electron/512x512.png",
+          executableName: "StackMonitor",
+          artifactName: "${productName}-${version}.${ext}",
+          category: "Utility",
+        },
+        win: {
+          icon: "./electron/512x512.png",
+        },
+      },
+    },
+  },
+};
