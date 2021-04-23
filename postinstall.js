03fbc2664394366f9e5fc792d5dc1972c2a0a300
clabroche <corentin.labroche@sailendra.fr>
2021-04-23 12:34:09 +0200

    fix(electron): not execute postinstall in npm environnement

--- postinstall.js
+++
@@ -1,8 +0,0 @@
-const commandExists = require('command-exists')
-const {execAsync} = require('./server/helpers/exec')
-
-;(async () => {
-  if(commandExists('electron-builder')) {
-    await execAsync('electron-builder install-app-deps')
-  }
-})()
