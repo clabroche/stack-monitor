9ec7dccefb1f63fa39a091ce93ac2e37ef7eae3f
clabroche <corentinlabroche@gmail.com>
2022-01-05 16:45:06 +0100

    fix(package.json): remove unused stuffs

--- src/helpers/socket.js
+++ src/helpers/socket.js
@@ -1,9 +1,8 @@
 import io from 'socket.io-client'
-import consola from 'consola'
 import ports from './ports';
 const socket = io('http://localhost:' + ports.socket)
 socket.on('connect', function () {
-  consola.log('Connection etablished')
+  console.log('Connection etablished')
 });
 
 
