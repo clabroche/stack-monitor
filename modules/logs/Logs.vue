9480d8b4b73bc76921eebe84e067dae00184c1c9
clabroche <corentinlabroche@gmail.com>
2023-02-13 23:10:34 +0100

    fix(logs): pass to vue 3

--- modules/logs/Logs.vue
+++ modules/logs/Logs.vue
@@ -48,13 +48,12 @@
   </div>
 </template>
 
-<script>
-import Stack from '@/models/stack'
+<script setup>
 import Socket from '@/helpers/socket';
 // @ts-ignore
 import { Terminal } from 'xterm/lib/xterm';
 import { FitAddon } from 'xterm-addon-fit';
-import SectionVue from '@/components/Section.vue';
+import sectionCmp from '@/components/Section.vue';
 import { SearchAddon } from 'xterm-addon-search';
 import { WebLinksAddon } from 'xterm-addon-web-links';
 import { CanvasAddon } from 'xterm-addon-canvas';
@@ -62,204 +61,186 @@ import debounce from 'debounce'
 import JsonViewer from 'vue-json-viewer'
 import jsonpath from 'jsonpath'
 import 'vue-json-viewer/style.css'
+import { computed, onMounted, ref, watch } from '@vue/runtime-core';
+const props = defineProps({
+  service: { default: null },
+  noStyle: { default: false },
+})
 
-export default {
-  name: 'Logs',
-  components: {
-    sectionCmp: SectionVue,
-    JsonViewer
-  },
-  props: {
-    service: { default: null },
-    noStyle: { default: false },
-  },
-  computed: {
-    watchChangeFilter() {
-      return `${this.isInclude}|${this.filterSearch}`;
-    },
-    jsonsToDisplay() {
-      this.scroll()
-      return this.jsonPathSearch ?
-        this.jsons.map(json => {
-          const res = jsonpath.query(json, this.jsonPathSearch)
-          if(res.length === 1) return res['0']
-          else res
-        })
-        : this.jsons
-    }
-  },
-  watch: {
-    async isOpen() {
-      this.createTerminal()
-    },
-    search() {
-      if (this.terminalSearch) {
-        this.terminalSearch.findNext(this.search)
-      }
-    },
-    watchChangeFilter() {
-      this.filter(this.filterSearch)
-    }
-  },
-  data() {
-    return {
-      terminal: null,
-      /** @type {SearchAddon | null} */
-      terminalSearch: null,
-      Stack,
-      filterSearch: '',
-      jsonPathSearch: '',
-      jsons: [],
-      isOpen: true,
-      isInclude: false,
-      logs: '',
-      json: '',
-      search: '',
-    }
-  },
-  async mounted() {
-    this.createTerminal()
-  },
-  methods: {
-    async createTerminal() {
-      await new Promise(resolve => setTimeout(resolve, 10));
-      if (!this.isOpen) return
-      const terminal = new Terminal({
-        smoothScrollDuration: 100,
-        experimentalCarAtlas: 'static',
-        fontFamily: 'MesloLGS NF, monospace',
-        convertEol: true,
-        disableStdin: true,
-        fontSize: 13,
-        allowTransparency: true,
-        minimumContrastRatio: 7,
-        theme: {
-          background: '#ffffff00',
-          foreground: '#4c4c4c',
-          selectionBackground: '#1d95db',
-          selectionForeground: 'white'
-        }
-      });
-      const fitAddon = new FitAddon();
-      terminal.attachCustomKeyEventHandler((arg) => {
-        if (arg.ctrlKey && arg.code === "KeyC" && arg.type === "keydown") {
-          const selection = terminal.getSelection();
-          if (selection) {
-            navigator?.clipboard?.writeText(selection)
-            return false;
-          }
-        }
-        return true;
-      });
-      terminal.loadAddon(fitAddon);
-      terminal.loadAddon(new WebLinksAddon());
-      terminal.loadAddon(new CanvasAddon());
+const terminal = ref(null)
+const terminalSearch = ref(null)
+const logsContainer = ref(null)
+const jsonsRef = ref(null)
+const filterSearch = ref('')
+const jsonPathSearch = ref('')
+const jsons = ref([])
+const isOpen = ref(true)
+const isInclude = ref(false)
+const logs = ref('')
+const json = ref('')
+const search = ref('')
 
-      this.terminal = terminal
-      terminal.open(this.$refs.logsContainer);
-      const searchAddon = new SearchAddon();
-      this.terminalSearch = searchAddon
-      terminal.loadAddon(searchAddon);
-      fitAddon.activate(terminal)
-      fitAddon.fit();
-      setTimeout(() => {
-        fitAddon.fit();
-      }, 100);
-      window.onresize = function () {
-        fitAddon.activate(terminal)
-        fitAddon.fit();
+
+watch(() => search.value, () => {
+  if (terminalSearch.value) {
+    terminalSearch.value.findNext(search.value)
+  }
+})
+watch(() => `${isInclude.value}|${filterSearch.value}`, () => filter(filterSearch.value))
+watch(() => isOpen.value, () => createTerminal()) 
+const filter = debounce(async function (message) {
+  terminal.value.clear()
+  jsons.value = []
+  logs.value
+    ?.split('\n')
+    ?.filter(line => {
+      if (!message) return true
+      const filters = message.includes(' | ')
+        ? message.split(' | ')
+        : [message]
+      return filters.filter(a => a).every((filter => {
+        const res = line.toUpperCase().match(new RegExp(escapeRegExp(filter), 'gi'))
+        return isInclude.value ? res : !res
+      }))
+    })
+    ?.map(line => {
+      write(line)
+      return line
+    }).join('\n');
+  await scroll()
+}, 100)
+
+onMounted(() => createTerminal())
+async function createTerminal() {
+  await new Promise(resolve => setTimeout(resolve, 10));
+  if (!isOpen.value) return
+  terminal.value = new Terminal({
+    smoothScrollDuration: 100,
+    experimentalCarAtlas: 'static',
+    fontFamily: 'MesloLGS NF, monospace',
+    convertEol: true,
+    disableStdin: true,
+    fontSize: 13,
+    allowTransparency: true,
+    minimumContrastRatio: 7,
+    theme: {
+      background: '#ffffff00',
+      foreground: '#4c4c4c',
+      selectionBackground: '#1d95db',
+      selectionForeground: 'white'
+    }
+  });
+  const fitAddon = new FitAddon();
+  terminal.value.attachCustomKeyEventHandler((arg) => {
+    if (arg.ctrlKey && arg.code === "KeyC" && arg.type === "keydown") {
+      const selection = terminal.value.getSelection();
+      if (selection) {
+        navigator?.clipboard?.writeText(selection)
+        return false;
       }
-      const logs = await this.service.getLogs()
-      logs.split('\n').map(line => this.write(line))
-      this.logs = logs
-      Socket.on('logs:update', data => {
-        if (data.label !== this.service.label || !data.msg) return
-        data.msg.trim().split('\n').filter(a => a).map(line => {
-          this.write(line)
-          this.logs += line
-          this.scroll()
-        })
-      })
-      Socket.on('logs:clear', data => {
-        if (data.label !== this.service.label) return
-        terminal.clear()
-      })
-      if (this.$refs.logsContainer) {
-        // @ts-ignore
-        this.$refs.logsContainer.scrollTo({
-          'behavior': 'smooth',
-          'left': 0,
-          // @ts-ignore
-          'top': this.$refs.logsContainer.offsetHeight + 1000000
-        });
-      }
-      setTimeout(async () => {
-        await this.scroll(true)
-      }, 100);
-    },
-    write(line) {
-      try { line = JSON.parse(line) } catch (error) { }
-      if (typeof line !== 'string') {
-        this.terminal.writeln(JSON.stringify(line))
-        this.jsons.push(line)
-        this.jsons = this.jsons.slice(-40)
-      } else {
-        this.terminal.writeln(line)
-      }
-    },
-    async clear() {
-      this.terminal.clear()
-      this.service.clear()
-      this.logs = ''
-      this.jsons = []
-    },
-    async nextSearch(ev) {
-      if (ev.shiftKey) {
-        return this.previousSearch()
-      }
-      if (this.terminalSearch) {
-        this.terminalSearch.findNext(this.search)
-      }
-    },
-    async previousSearch(ev) {
-      if (this.terminalSearch) {
-        this.terminalSearch.findPrevious(this.search)
-      }
-    },
-    escapeRegExp(text) {
-      return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
-    },
-    async scroll(force) {
-      const container = this.$refs.jsonsRef
-      if(!container) return
-      const shouldScroll = container.scrollTop + container.clientHeight === container.scrollHeight
-      await new Promise(res => setTimeout(res, 100))
-      if (shouldScroll || force) container.scrollTop = container.scrollHeight
-    },
-    filter: debounce(async function (message) {
-      this.terminal.clear()
-      this.jsons = []
-      this.logs
-        ?.split('\n')
-        ?.filter(line => {
-          if (!message) return true
-          const filters = message.includes(' | ')
-            ? message.split(' | ')
-            : [message]
-          return filters.filter(a => a).every((filter => {
-            const res = line.toUpperCase().match(new RegExp(this.escapeRegExp(filter), 'gi'))
-            return this.isInclude ? res : !res
-          }))
-        })
-        ?.map(line => {
-          this.write(line)
-          return line
-        }).join('\n');
-        await this.scroll()
-    }, 100)
+    }
+    return true;
+  });
+  terminal.value.loadAddon(fitAddon);
+  terminal.value.loadAddon(new WebLinksAddon());
+  terminal.value.loadAddon(new CanvasAddon());
+
+  terminal.value.open(logsContainer.value);
+  const searchAddon = new SearchAddon();
+  terminalSearch.value = searchAddon
+  terminal.value.loadAddon(searchAddon);
+  fitAddon.activate(terminal)
+  fitAddon.fit();
+  setTimeout(() => {
+    fitAddon.fit();
+  }, 100);
+  window.onresize = function () {
+    fitAddon.activate(terminal)
+    fitAddon.fit();
+  }
+  logs.value = await props.service.getLogs()
+  logs.value.split('\n').map(line => write(line))
+  Socket.on('logs:update', data => {
+    if (data.label !== props.service.label || !data.msg) return
+    data.msg.trim().split('\n').filter(a => a).map(line => {
+      write(line)
+      logs.value += line
+      scroll()
+    })
+  })
+  Socket.on('logs:clear', data => {
+    if (data.label !== props.service.label) return
+    terminal.value.clear()
+  })
+  if (logsContainer.value) {
+    // @ts-ignore
+    logsContainer.value.scrollTo({
+      'behavior': 'smooth',
+      'left': 0,
+      // @ts-ignore
+      'top': logsContainer.value.offsetHeight + 1000000
+    });
+  }
+  setTimeout(async () => {
+    await scroll(true)
+  }, 100);
+}
+
+const jsonsToDisplay = computed(() => {
+  scroll()
+  return jsonPathSearch.value ?
+    jsons.value.map(json => {
+      const res = jsonpath.query(json, jsonPathSearch.value)
+      if (res.length === 1) return res['0']
+      else res
+    })
+    : jsons.value
+})
+
+
+
+function write(line) {
+  try { line = JSON.parse(line) } catch (error) { }
+  if (typeof line !== 'string') {
+    terminal.value.writeln(JSON.stringify(line))
+    jsons.value.push(line)
+    jsons.value = jsons.value.slice(-40)
+  } else {
+    terminal.value.writeln(line)
   }
 }
+
+async function clear() {
+  terminal.value.clear()
+  props.service.clear()
+  logs.value = ''
+  jsons.value = []
+}
+async function nextSearch(ev) {
+  if (ev.shiftKey) {
+    return previousSearch()
+  }
+  if (terminalSearch.value) {
+    terminalSearch.value.findNext(search.value)
+  }
+}
+async function previousSearch(ev) {
+  if (terminalSearch.value) {
+    terminalSearch.value.findPrevious(search.value)
+  }
+}
+function escapeRegExp(text) {
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+}
+async function scroll(force) {
+  const container = jsonsRef.value
+  if(!container) return
+  const shouldScroll = container.scrollTop + container.clientHeight === container.scrollHeight
+  await new Promise(res => setTimeout(res, 100))
+  if (shouldScroll || force) container.scrollTop = container.scrollHeight
+}
 </script>
+
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
