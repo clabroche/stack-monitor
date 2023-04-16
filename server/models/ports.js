f04f250e24e04b8ca738249f64a933947bc5f4cc
clabroche <corentinlabroche@gmail.com>
2023-04-17 00:30:33 +0200

    fix(index.html): dont need to inject port anymore

--- server/models/ports.js
+++ server/models/ports.js
@@ -7,19 +7,11 @@ module.exports = {
   /** @param {number} port */
   setHttpPort(port) {
     if (!this.http) this.http = port
-    inject(`<script>httpPort = ${this.http}</script>`)
   },
   cleanHtml() {
     if (existsSync(indexPath)) {
       const index = fse.readFileSync(indexPath, 'utf-8')
       fse.writeFileSync(indexPath, index.split('\n').pop() || '', 'utf-8')
     }
-  }
-}
-/** @param {string} html */
-function inject(html) {
-  if (existsSync(indexPath)) {
-    const index = fse.readFileSync(indexPath, 'utf-8')
-    fse.writeFileSync(indexPath, html + '\n' + index, 'utf-8')
   }
 }
