d38eb98f31cc3c2e127ec59396bfe26413d51b28
clabrocheAddWorking <corentin@addworking.com>
2023-04-17 00:37:29 +0200

    fix(index.html): or to clean htmlfile

--- server/models/ports.js
+++ server/models/ports.js
@@ -1,17 +1,7 @@
-const { existsSync } = require('fs-extra')
-const fse = require('fs-extra')
-const pathfs = require('path')
-const indexPath = pathfs.resolve(__dirname, '..', 'public', 'index.html')
 module.exports = {
   http: process.env.HTTP_PORT || 0,
   /** @param {number} port */
   setHttpPort(port) {
     if (!this.http) this.http = port
   },
-  cleanHtml() {
-    if (existsSync(indexPath)) {
-      const index = fse.readFileSync(indexPath, 'utf-8')
-      fse.writeFileSync(indexPath, index.split('\n').pop() || '', 'utf-8')
-    }
-  }
 }
