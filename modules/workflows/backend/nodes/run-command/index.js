5eff16e95ef4578eb71abe3ee45e26dc9d4c2c4f
clabrocheAddWorking <corentin@addworking.com>
2025-03-21 23:53:06 +0100

    fix(nodered): debug run command

--- modules/workflows/backend/nodes/run-command/index.js
+++ modules/workflows/backend/nodes/run-command/index.js
@@ -7,26 +7,28 @@ module.exports = function (RED) {
     if (!config.commandId) config.commandId = 'STACK_LOAD_FROM_INPUT'
     const { sockets, stack } = RED.settings.functionGlobalContext.stackmonitor
     node.on('input', async function (msg) {
-      let command, service
-      stack.getServices().forEach(_service => {
-        [..._service.commands, ..._service.shortcuts].forEach(_command => {
-          if (_command.id === config.commandId) {
-            command = _command
-            service = _service
-          }
+      try {
+        let command, service
+        stack.getServices().forEach(_service => {
+          [..._service.commands, ..._service.shortcuts].forEach(_command => {
+            if (_command.id === config.commandId) {
+              command = _command
+              service = _service
+            }
+          });
         });
-      });
-      if(!command) {
-        node.status({ fill: "red", shape: "ring", text: "No command found" });
-        return
+        if(!command) {
+          node.status({ fill: "red", shape: "ring", text: "No command found" });
+          return
+        }
+        const axios = await stack.getAxios({})
+        await axios.post(`/logs/${service.label}/prompt`, { command, service: service.label })
+        node.status({ fill: "green", shape: "ring", text: `OK - ${new Date().toISOString()}` });
+        return node.send(msg)
+      } catch (err) {
+        node.error(err + (err?.stack || ''))
+        node.status({ fill: "red", shape: "ring", text: err?.message || err });
       }
-      const axios = await stack.getAxios({})
-      return axios.post(`/logs/${service.label}/prompt`, { command, service: service.label })
-        .then(() =>{
-          node.status({ fill: "green", shape: "ring", text: `OK - ${new Date().toISOString()}` });
-        }).catch(err => {
-          node.status({ fill: "red", shape: "ring", text: err?.message || err });
-        })
     })
   }
 
